8321def49f923ffaf5634b88431c05032542ccc2
Reghan Anthony <263368686+ReghanAnt@users.noreply.github.com.>
2026-05-02 01:31:16 +0200

    Write the logic to play a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -22,3 +22,23 @@ console.log(getHumanChoice());
 let humanScore = 0;
 let computerScore = 0; 
 
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+      console.log("It's a draw!");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+
+    console.log(`Score: Human ${humanScore} - Computer ${computerScore}`);
+  }
